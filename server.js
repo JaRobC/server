8dea258acd74cfe14a40f60e8565c5d8860eb86a
Barsan Robert <67766043+JaRobC@users.noreply.github.com>
2023-05-16 10:23:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,14 +12,6 @@ app.use(cors());
 app.use(express.json());
 config();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://learnsi.onrender.com/;
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
-
 
 /*conn file*/
 import connect from './database/conn.js';
