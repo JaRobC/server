20a39031ea078948094efdf722503725cf3c4dcb
Barsan Robert <67766043+JaRobC@users.noreply.github.com>
2023-05-16 08:37:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 config();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://learnsi.onrender.com/;
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
